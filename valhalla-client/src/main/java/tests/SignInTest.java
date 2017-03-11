dbc3bbcc91e1f105a357a477aab4e5261de97859
MoaBK <MoaBK@DESKTOP-FS06UMA>
2017-03-11 17:13:20 +0100

    Projet Client
    Change-Id: I32e6277ae2305eeb8d3babd6970540e63fe6b71e
    Signed-off-by: MoaBK <MoaBK@DESKTOP-FS06UMA>

---
+++ valhalla-client/src/main/java/tests/SignInTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import tn.esprit.bzbz.valhalla.services.signin.SignInServices;
+import tn.esprit.bzbz.valhalla.services.signin.SignInServicesRemote;
+
+public class SignInTest {
+
+	public static void main(String[] args) throws NamingException {
+		Context context = new InitialContext();
+		SignInServicesRemote sisr = (SignInServicesRemote) context.lookup(
+				"valhalla-ear/valhalla-ejb/SignInServices!tn.esprit.bzbz.valhalla.services.signin.SignInServicesRemote");
+		System.out.println("ID: " + sisr.signIn("mbk", "mbk"));
+		System.out.println("IDC:" + SignInServices.userConnecte);
+
+	}
+
+}
