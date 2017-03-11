71010137686af6e477c72a5532c7def843cdf83b
raouf ben fadhel <abderraouf.benfadhel@esprit.tn>
2017-03-11 12:32:35 +0100

    test
    Change-Id: I3112b75a81379c758d510aa67519caeae4710bce
    Signed-off-by: MoaBK <MoaBK@DESKTOP-FS06UMA>

---
+++ valhalla-ejb/src/main/java/tn/esprit/bzbz/valhalla/entity/ReportComment.java
@@ -0,0 +1,26 @@
+package tn.esprit.bzbz.valhalla.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+
+import tn.esprit.bzbz.valhalla.entity.embeddable.CommentId;
+import tn.esprit.bzbz.valhalla.entity.embeddable.ReportCommentId;
+
+/**
+ * Entity implementation class for Entity: ReportSubject
+ *
+ */
+//@Entity
+
+public class ReportComment implements Serializable {
+
+	
+		
+
+}
---
+++ valhalla-ejb/src/main/java/tn/esprit/bzbz/valhalla/entity/embeddable/ReportCommentId.java
@@ -0,0 +1,13 @@
+package tn.esprit.bzbz.valhalla.entity.embeddable;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class ReportCommentId implements Serializable {
+	
+	
+
+}
